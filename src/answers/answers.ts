91d66080ff3271ce1579af06c5f9cf08300bb3be
Joel Tariku <jojotk10@gmail.com>
2026-04-13 00:26:55 -0400

    moved start date out of getDailyWord function since this is a constant date. Also renamed present variable to presentDate for clarity.

--- src/answers/answers.ts
+++ src/answers/answers.ts
@@ -1,11 +1,11 @@
 import { ANSWERS } from "../guesses/guesses"
 
 const millisecondsPerDay = 86400000
+const startDate = new Date('2026-04-12T00:00:00').getTime()
 
 export const getDailyWord = (): string => {
-    const start = new Date('2026-04-12T00:00:00').getTime()
-    const present = Date.now()
-    const dayIndex = Math.floor((present - start) / millisecondsPerDay)
+    const presentDate = Date.now()
+    const dayIndex = Math.floor((presentDate - startDate) / millisecondsPerDay)
     const answer = ANSWERS[dayIndex % ANSWERS.length]
     return answer;
 }              
