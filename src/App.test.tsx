7a44e22e8ab1ce1e3f7a8effd728e01a986bd63e
Joel Tariku <jojotk10@gmail.com>
2026-04-15 17:20:32 -0400

    valid submitted guesses colored based on position and correctness of letters.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import {act, fireEvent, render, screen} from '@testing-library/react';
+import {act, fireEvent, render, screen, within} from '@testing-library/react';
 import App from './App';
 import { describe, expect, it, beforeEach, afterEach, vi } from 'vitest';   
 
@@ -9,7 +9,7 @@ vi.mock('./answers/answers.ts', () => ({
 describe("App", () => {
     it("renders the App", () => {
         render(<App />)
-        const rows = screen.getAllByTestId("board-row")
+        const rows = screen.getAllByTestId(/^board-row-/);
         expect(rows).toHaveLength(6);
     })
     describe("error messages", () => {
@@ -88,6 +88,7 @@ describe("App", () => {
     describe('Guesses', () => {
         const mockAnswer = "APPLE"
         const wrongAnswer = "TOXIN"
+        const wrongAnswer2 = "ZONES"
         describe('Correct guesses', () => {
             it('does nothing for key presses after guessing correctly first try', () => {
                 render(<App />)
@@ -98,8 +99,8 @@ describe("App", () => {
 
                 fireEvent.keyDown(window, { key: 'A' })
 
-                const rows = screen.getAllByTestId('board-row')
-                expect(rows[1].textContent).toBe('')
+                const row = screen.getByTestId('board-row-1')
+                expect(row.textContent).toBe('')
             })
             it('does nothing for "Enter" key press after guessing correctly on last try', () => {
                 render(<App />)
@@ -139,6 +140,54 @@ describe("App", () => {
                 fireEvent.keyDown(window, { key: 'Enter' })
 
                 expect(screen.queryByText("Not enough letters!")).not.toBeInTheDocument()
+            })
+            it('makes every letter green after submitting', () => {
+                render(<App />)
+
+                mockAnswer.split('').forEach(letter => {
+                    fireEvent.keyDown(window, { key: letter })
+                })
+                fireEvent.keyDown(window, { key: 'Enter' })
+
+                const row0 = screen.getByTestId('board-row-0')
+                const letters = within(row0).getAllByTestId(/^letter-/)
+
+                letters.forEach(letter => {
+                    expect(letter).toHaveClass('green')
+                })
+            })
+            it('does not add color to letters for correct guess before submitted', () => {
+                render(<App />)
+
+                mockAnswer.split('').forEach(letter => {
+                    fireEvent.keyDown(window, { key: letter })
+                })
+
+                const row0 = screen.getByTestId('board-row-0')
+                const letters = within(row0).getAllByTestId(/^letter-/)
+
+                letters.forEach(letter => {
+                    expect(letter).not.toHaveClass('green')
+                })
+            })
+        })
+        describe('Incorrect guesses', () => {
+            it('after submitting valid incorrect guess, colors of each letter are as expected', () => {
+                render(<App />)
+                
+                wrongAnswer2.split('').forEach(letter => {
+                    fireEvent.keyDown(window, { key: letter })
+                })
+                fireEvent.keyDown(window, { key: 'Enter' })
+
+                const row0 = screen.getByTestId('board-row-0')
+                const letters = within(row0).getAllByTestId(/^letter-/)
+
+                expect(letters[0]).toHaveClass('dark-grey')
+                expect(letters[1]).toHaveClass('dark-grey')
+                expect(letters[2]).toHaveClass('dark-grey')
+                expect(letters[3]).toHaveClass('yellow')
+                expect(letters[4]).toHaveClass('dark-grey')
             })
         })
     })
--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -5,15 +5,22 @@ type GameboardProps = {
     guesses: string[];
     currentGuess: string;
     errorMessage: string;
+    answer: string;
 }
 
-export default function Gameboard({ guesses, currentGuess, errorMessage }: GameboardProps) { 
+export default function Gameboard({ guesses, currentGuess, errorMessage, answer }: GameboardProps) { 
     return (
         <div className="gameboard-container">
             <div className="gameboard">
                 {errorMessage && <ErrorMessage message={errorMessage}/>}
                 {Array.from({ length: 6}).map((_, i) => (
-                    <GuessRow key={i} guess={i < guesses.length ? guesses[i] : (i === guesses.length ? currentGuess : '')} />
+                    <GuessRow 
+                        key={i}
+                        rowIndex={i} 
+                        guess={i < guesses.length ? guesses[i] : (i === guesses.length ? currentGuess : '')} 
+                        answer={answer} 
+                        isSubmitted={i < guesses.length}
+                    />
                 ))}
             </div>
         </div>
