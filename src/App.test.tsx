8edbe5adceb291f018c6159c30d03960bb62fbf1
Joel Tariku <69915522+joeltariku@users.noreply.github.com>
2026-04-17 16:59:27 -0400

    Renamed ErrorMessage component to Message. (#20)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -12,77 +12,79 @@ describe("App", () => {
         const rows = screen.getAllByTestId(/^board-row-/);
         expect(rows).toHaveLength(6);
     })
-    describe("error messages", () => {
-        beforeEach(() => {
-            vi.useFakeTimers()
-        })
-        afterEach(() => {
-            vi.useRealTimers()
-        })
-        it("shows correct error message for entering guess with less than 5 letters", async () => {
-            // const user = userEvent.setup({ 
-            //     advanceTimers: (delay) => vi.advanceTimersByTime(delay) 
-            // });
-            render(<App />)
-
-            //await user.keyboard('ABC{Enter}')
-
-            fireEvent.keyDown(window, {key: 'A'})
-            fireEvent.keyDown(window, {key: 'B'})
-            fireEvent.keyDown(window, {key: 'C'})
-            fireEvent.keyDown(window, {key: 'Enter'})
-
-            expect(screen.getByText("Not enough letters!")).toBeInTheDocument()
-
-            await act(async () => {
-                vi.advanceTimersByTime(1000)
+    describe("Messages", () => {
+         describe("error messages", () => {
+            beforeEach(() => {
+                vi.useFakeTimers()
             })
-
-            expect(screen.queryByText("Not enough letters!")).not.toBeInTheDocument()
-        })
-        it("shows correct error message for entering guess which isn't valid", async () => {
-            // const user = userEvent.setup({ 
-            //     advanceTimers: (delay) => vi.advanceTimersByTime(delay) 
-            // });
-            render(<App />)
-
-            //await user.keyboard('ABC{Enter}')
-
-            fireEvent.keyDown(window, { key: 'A' })
-            fireEvent.keyDown(window, { key: 'B' })
-            fireEvent.keyDown(window, { key: 'C' })
-            fireEvent.keyDown(window, { key: 'D' })
-            fireEvent.keyDown(window, { key: 'E' })
-            fireEvent.keyDown(window, { key: 'Enter' })
-
-            expect(screen.getByText("Not a valid guess!")).toBeInTheDocument()
-
-            await act(async () => {
-                vi.advanceTimersByTime(1000)
+            afterEach(() => {
+                vi.useRealTimers()
             })
+            it("shows correct error message for entering guess with less than 5 letters", async () => {
+                // const user = userEvent.setup({ 
+                //     advanceTimers: (delay) => vi.advanceTimersByTime(delay) 
+                // });
+                render(<App />)
 
-            expect(screen.queryByText("Not a valid guess!")).not.toBeInTheDocument()
-        })
-        it("error message isn't shown for less than 1 second", async () => {
-            // const user = userEvent.setup({ 
-            //     advanceTimers: (delay) => vi.advanceTimersByTime(delay) 
-            // });
-            render(<App />)
+                //await user.keyboard('ABC{Enter}')
 
-            //await user.keyboard('ABC{Enter}')
+                fireEvent.keyDown(window, {key: 'A'})
+                fireEvent.keyDown(window, {key: 'B'})
+                fireEvent.keyDown(window, {key: 'C'})
+                fireEvent.keyDown(window, {key: 'Enter'})
 
-            fireEvent.keyDown(window, {key: 'A'})
-            fireEvent.keyDown(window, {key: 'B'})
-            fireEvent.keyDown(window, {key: 'C'})
-            fireEvent.keyDown(window, {key: 'Enter'})
+                expect(screen.getByText("Not enough letters!")).toBeInTheDocument()
 
-            expect(screen.getByText("Not enough letters!")).toBeInTheDocument()
+                await act(async () => {
+                    vi.advanceTimersByTime(1000)
+                })
 
-            await act(async () => {
-                vi.advanceTimersByTime(999)
+                expect(screen.queryByText("Not enough letters!")).not.toBeInTheDocument()
             })
+            it("shows correct error message for entering guess which isn't valid", async () => {
+                // const user = userEvent.setup({ 
+                //     advanceTimers: (delay) => vi.advanceTimersByTime(delay) 
+                // });
+                render(<App />)
 
-            expect(screen.queryByText("Not enough letters!")).toBeInTheDocument()
+                //await user.keyboard('ABC{Enter}')
+
+                fireEvent.keyDown(window, { key: 'A' })
+                fireEvent.keyDown(window, { key: 'B' })
+                fireEvent.keyDown(window, { key: 'C' })
+                fireEvent.keyDown(window, { key: 'D' })
+                fireEvent.keyDown(window, { key: 'E' })
+                fireEvent.keyDown(window, { key: 'Enter' })
+
+                expect(screen.getByText("Not a valid guess!")).toBeInTheDocument()
+
+                await act(async () => {
+                    vi.advanceTimersByTime(1000)
+                })
+
+                expect(screen.queryByText("Not a valid guess!")).not.toBeInTheDocument()
+            })
+            it("error message isn't shown for less than 1 second", async () => {
+                // const user = userEvent.setup({ 
+                //     advanceTimers: (delay) => vi.advanceTimersByTime(delay) 
+                // });
+                render(<App />)
+
+                //await user.keyboard('ABC{Enter}')
+
+                fireEvent.keyDown(window, {key: 'A'})
+                fireEvent.keyDown(window, {key: 'B'})
+                fireEvent.keyDown(window, {key: 'C'})
+                fireEvent.keyDown(window, {key: 'Enter'})
+
+                expect(screen.getByText("Not enough letters!")).toBeInTheDocument()
+
+                await act(async () => {
+                    vi.advanceTimersByTime(999)
+                })
+
+                expect(screen.queryByText("Not enough letters!")).toBeInTheDocument()
+            })
         })
     })
     describe('Guesses', () => {
--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -1,18 +1,18 @@
-import ErrorMessage from "./ErrorMessage";
+import Message from "./Message";
 import GuessRow from "./GuessRow";
 
 type GameboardProps = {
     guesses: string[];
     currentGuess: string;
-    errorMessage: string;
+    message: string;
     answer: string;
 }
 
-export default function Gameboard({ guesses, currentGuess, errorMessage, answer }: GameboardProps) { 
+export default function Gameboard({ guesses, currentGuess, message, answer }: GameboardProps) { 
     return (
         <div className="gameboard-container">
             <div className="gameboard">
-                {errorMessage && <ErrorMessage message={errorMessage}/>}
+                {message && <Message message={message}/>}
                 {Array.from({ length: 6}).map((_, i) => (
                     <GuessRow 
                         key={i}
