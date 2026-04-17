c8e6509c77515907b2b9e19a1dc1e97284e9996d
Joel Tariku <jojotk10@gmail.com>
2026-04-16 23:17:31 -0400

    added key press functionality to keyboard display keys.

--- src/components/LetterKey.tsx
+++ src/components/LetterKey.tsx
@@ -1,14 +1,16 @@
 type KeyProps = {
     content: string;
     color: string;
+    handleKeyPress: (key: string) => void;
 }
 
-export default function LetterKey({ content, color }: KeyProps) {
+export default function LetterKey({ content, color, handleKeyPress }: KeyProps) {
     const isLetter = content.length === 1 && content.match(/[a-z]/i);
     return (
         <button 
             className={`key ${isLetter ? 'letter-key' : 'not-letter-key'} ${color}`}
             data-testid={`key-${content}`}
+            onClick={() => handleKeyPress(content)}
         >
             {content}
         </button>
