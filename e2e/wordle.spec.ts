62905b5d52cd1d1670463ec2c8c35bc8701389c6
Joel Tariku <jojotk10@gmail.com>
2026-04-18 17:38:28 -0400

    Installed playwright and added test.

---
+++ e2e/wordle.spec.ts
@@ -0,0 +1,26 @@
+import test, { expect } from "@playwright/test";
+
+test('has correct title', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await expect(page).toHaveTitle('wordle')
+})
+test('button click does not take focus from the window', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    const keyA = page.getByTestId('key-A')
+    const keyP = page.getByTestId('key-P')
+    const keyL = page.getByTestId('key-L')
+    const keyE = page.getByTestId('key-E')
+
+    await keyA.click()
+    await keyP.click()
+    await keyP.click()
+    await keyL.click()
+    await keyE.click()
+
+    page.keyboard.press('Enter')
+
+    const row1 = page.getByTestId('board-row-1')
+    await expect(row1).toHaveText('')
+})
