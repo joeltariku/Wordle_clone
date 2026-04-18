8d43fe240e9c923ac50a45a5f76a2bbf18b6ee60
Joel Tariku <jojotk10@gmail.com>
2026-04-18 00:31:02 -0400

    Added fix for button being clicked again after clicking physical enter button.

--- src/components/LetterKey.tsx
+++ src/components/LetterKey.tsx
@@ -11,6 +11,7 @@ export default function LetterKey({ content, color, handleKeyPress }: KeyProps) 
             className={`key ${isLetter ? 'letter-key' : 'not-letter-key'} ${color}`}
             data-testid={`key-${content}`}
             onClick={() => handleKeyPress(content)}
+            onMouseDown={(e) => e.preventDefault()}
         >
             {content}
         </button>
